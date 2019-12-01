ecd9f78b029e93732f552b7ecca0bdd5b746a129
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-11-30 20:17:24 -0500

    comment out ones i'm unsure about

--- language_servers.sh
+++ language_servers.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # Go
-# https://github.com/sourcegraph/go-langserver
-# Doesn't have autocomplete yet :/
-# go get -u github.com/sourcegraph/go-langserver
+# https://github.com/golang/tools/blob/master/gopls/doc/user.md
+go get golang.org/x/tools/gopls@latest
 
 # Rubies
 # https://github.com/castwide/solargraph
@@ -12,9 +11,9 @@ yard config --gem-install-yri
 
 # Rust
 # https://github.com/rust-lang-nursery/rls
-rustup update
-rustup component add rls-preview rust-analysis rust-src
+# rustup update
+# rustup component add rls-preview rust-analysis rust-src
 
 # Javascript/Typescript
 # https://github.com/sourcegraph/javascript-typescript-langserver
-npm i -g javascript-typescript-langserver
+# npm i -g javascript-typescript-langserver
