0480072042ab57b384fec24d10e7054e862b1a4c
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-01-10 10:39:32 -0500

    fix autohide url in firefox 72

--- nvim.sh
+++ nvim.sh
@@ -4,8 +4,5 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# https://github.com/Shougo/deoplete.nvim
-pip3 install --user pynvim
-
 # Run installs
 nvim +PlugInstall +UpdateRemotePlugins +qa
